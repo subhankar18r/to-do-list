5dcc2d548d7773faded7c214695d97e624250df6
Subhankar Rajbanshi <subhankar18r@gmail.com>
2024-10-26 13:57:09 +0530

    completed task feature finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const taskInput = document.querySelector(".taskInput");
 const tasksContainer = document.querySelector(".not_completed");
+const completedTasks = document.querySelector(".completed");
+const clearBtn = document.querySelector(".cleat_btn");
 
 function addTask() {
   if (!taskInput.value) {
@@ -15,23 +17,39 @@ function addTask() {
 
 tasksContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
+    completedTasks.appendChild(e.target);
   } else if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
   }
   saveData();
 });
 
+completedTasks.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    tasksContainer.appendChild(e.target);
+  } else if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
+  saveData();
+});
+
+function deleteCompletedTasks() {
+  completedTasks.innerHTML = "";
+  saveData();
+}
+
 function taskHtml(task) {
   return `${task} <img src="./images/cross.svg" alt="cross" />`;
 }
 
 function saveData() {
-  localStorage.setItem("data", tasksContainer.innerHTML);
+  localStorage.setItem("tasks", tasksContainer.innerHTML);
+  localStorage.setItem("completed_tasks", completedTasks.innerHTML);
 }
 
 function fetchData() {
-  tasksContainer.innerHTML = localStorage.getItem("data");
+  tasksContainer.innerHTML = localStorage.getItem("tasks");
+  completedTasks.innerHTML = localStorage.getItem("completed_tasks");
 }
 
-// fetchData();
+fetchData();
